b01068504c815fd90461cb2d82fbfb8f09710229
makharadze <makharadze_ks@ascon.ru>
2019-04-01 18:38:24 +0300

    Initial Commit

---
+++ backend/MathExample/Calc.php
@@ -0,0 +1,9 @@
+<?php namespace TeamFinder\MathExample;
+
+class Calc 
+{
+    public static function Add(int $num1, int $num2)
+    {
+        return $num1 + $num2;
+    }
+}
---
+++ tests/backend/MathExampleTest.php
@@ -0,0 +1,19 @@
+<?php namespace backend\tests;
+
+use PHPUnit\Framework\TestCase;
+use TeamFinder\MathExample\Calc;
+
+
+class MathExampleTest extends TestCase
+{
+    public function testAdd()
+    {
+        $this->assertTrue(true);
+        $this->assertEquals(3, Calc::Add(2, 1));
+    }
+
+    public function testAddFail()
+    {
+        $this->assertEquals(3, Calc::Add(2, 4), "FAIL EXPECTED!");
+    }
+}
