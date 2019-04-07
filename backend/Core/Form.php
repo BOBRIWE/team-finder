1d23db84ab26220ae32a4e19196a527120613020
BOBRIWE <kirill.madze@gmail.com>
2019-04-07 22:43:33 +0300

    index.php form

---
+++ backend/Core/Form.php
@@ -0,0 +1,27 @@
+<?php namespace TeamFinder\Core;
+
+class Form
+{
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+}
